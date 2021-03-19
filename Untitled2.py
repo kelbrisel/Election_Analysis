f1d3bd7c2700c335aab7b1d4bc8898f1a4dcb0f3
Kelly <78561980+kelbrisel@users.noreply.github.com>
2021-03-19 01:28:02 -0500

    adding the election audit code.

---
+++ Untitled2.py
@@ -0,0 +1,15 @@
+
+## Instructions
+
+#* Using a while loop, ask the user "How many numbers?", then print out a chain of ascending numbers, starting at 0.
+print("How many numbers?")
+range(0,1,2,3,4,5,6,7,8,9,10,11)
+#* After the results have printed ask the user if they would like to continue.
+while 
+  #* If "y", restart the process, starting at 0 again.
+for i in range(y) print 
+  #* If "n", exit the chain.
+
+## Bonus
+
+#* Rather than just displaying numbers constantly starting at 0, have the numbers begin at the end of the previous chain.
